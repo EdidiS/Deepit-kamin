e2747cffa0c3b3e28357515eb7a4153bddacbeff
EdidiS <83082275+EdidiS@users.noreply.github.com>
2025-08-30 06:26:59 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -3,6 +3,7 @@ let hamburgerIcon = document.querySelector('.fa-bars');
 let closeIcon = document.querySelector('.fa-xmark');
 let navbarMenu = document.querySelector('.navbar-menu');
 let navbarToggle = document.querySelector('.navbar-toggle');
+let nav = document.querySelector('#nav');
 // let navbar = document.querySelector('#nav');
 
 
@@ -11,9 +12,22 @@ navbarToggle.addEventListener('click', function () {
     hamburgerIcon.classList.toggle('un-active');
     closeIcon.classList.toggle('active');
 })
-// window.addEventListener('scroll', () => {
-//     nav.classList.toggle('shadow', window.scrollY > 0);
-// });
+window.addEventListener('scroll', () => {
+    nav.classList.toggle('newNav', window.scrollY > 0);
+    navbarMenu.classList.remove('active-toggle');
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar-menu a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            navbarMenu.classList.remove('active-toggle');
+            hamburgerIcon.classList.toggle('un-active');
+            closeIcon.classList.toggle('active');
+        })
+    })
+
+})
 // Remove Menu On Scroll
 // window.onscroll = () => {
 //     menu.classList.remove('is-active');
